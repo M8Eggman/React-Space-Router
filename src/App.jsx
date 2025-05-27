0fd0c0ada0680fb5f6854fa32e97a301dc3ab26c
GoNEAR <cagatay.morgul@outlook.com>
2025-05-27 13:26:22 +0200

    page home finis

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-// import layout
-import TechnologyLayout from "./pages/technology/TechnologyLayout";
-import CrewLayout from "./pages/crew/CrewLayout";
-import DestinationLayout from "./pages/destination/DestinationLayout";
+import { Navigate, Route, Routes } from "react-router-dom";
 // import page
 import Home from "./pages/home/Home";
 import DestinationContent from "./pages/destination/DestinationContent";
 import CrewContent from "./pages/crew/CrewContent";
 import TechnologyContent from "./pages/technology/TechnologyContent";
+import Nav from "./components/nav/Nav";
 
 export default function App() {
   return (
     <>
+      <Nav />
       <Routes>
         <Route path="/React-Space-Router" element={<Home />} />
         {/* Destination */}
-        <Route path="/React-Space-Router/destination" element={<DestinationLayout />}>
-          <Route index element={<DestinationContent defaultValue="moon" />} />
-          <Route path=":destination" element={<DestinationContent  />} />
+        <Route path="/React-Space-Router/destination">
+          <Route index element={<DestinationContent />} />
+          <Route path=":destination" element={<DestinationContent />} />
         </Route>
         {/* Crew */}
-        <Route path="/React-Space-Router/crew" element={<CrewLayout />}>
-          <Route index element={<CrewContent defaultValue="douglas" />} />
+        <Route path="/React-Space-Router/crew">
+          <Route index element={<DestinationContent />} />
           <Route path=":name" element={<CrewContent />} />
         </Route>
         {/* Technology */}
-        <Route path="/React-Space-Router/technology" element={<TechnologyLayout />}>
-          <Route index element={<TechnologyContent defaultValue="vehicle" />} />
+        <Route path="/React-Space-Router/technology">
+          <Route index element={<DestinationContent />} />
           <Route path=":technology" element={<TechnologyContent />} />
         </Route>
       </Routes>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,21 @@ import "./Home.css";
 export default function Home() {
   return (
     <>
-      <Nav />
+      <section className="sectionHome">
+        <div className="home">
+          <div className="homeText">
+            <p>SO, YOU WANT TO TRAVEL TO</p>
+            <h1>SPACE</h1>
+            <p>
+              Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because
+              we’ll give you a truly out of this world experience!
+            </p>
+          </div>
+          <div className="homeButton">
+            <button>EXPLORE</button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
