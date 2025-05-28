7f6482c25ed2e0695a57ae004fb1d88ff0f69ed9
GoNEAR <cagatay.morgul@outlook.com>
2025-05-28 09:51:23 +0200

    page crew et sous page fini

--- src/pages/crew/CrewContent.jsx
+++ src/pages/crew/CrewContent.jsx
@@ -1,14 +1,34 @@
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import "./CrewContent.css";
 import dataCrew from "../../data/dataCrew.json";
 
 export default function CrewContent() {
-  const { destination } = useParams();
-  const data = dataCrew.find((item) => item.link === destination);
+  const { name } = useParams();
+  const data = dataCrew.find((item) => item.link.toLowerCase() === name.toLowerCase());
   return (
     <>
       <section className="sectionCrew">
-        <div className="crew"></div>
+        <div className="crew">
+          <div className="crewContent">
+            <div className="crewContentDescription">
+              <p>{data.role.toUpperCase()}</p>
+              <h3>{data.name.toUpperCase()}</h3>
+              <p>{data.bio}</p>
+            </div>
+            <nav>
+              <ul>
+                {dataCrew.map((item) => (
+                  <li>
+                    <NavLink to={`/React-Space-Router/crew/${item.link}`}></NavLink>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+          <div className="crewImg">
+            <img src={data.image} alt="" />
+          </div>
+        </div>
       </section>
     </>
   );
