1d695b7a8049cdb51d6b1e453f0e79d9bba37a7b
GoNEAR <cagatay.morgul@outlook.com>
2025-05-28 12:42:52 +0200

    jsp

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,21 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/React-Space-Router" element={<Home />} />
+        <Route path="/React-Space-Router/" element={<Home />} />
         {/* Destination */}
-        <Route path="/React-Space-Router/destination" element={<DestinationLayout />}>
+        <Route path="/React-Space-Router/destination/" element={<DestinationLayout />}>
           <Route index element={<Navigate to="moon" />} /> {/* navigate attribu une route par défaut dans ce cas-ci "moon"  */}
-          <Route path=":destination" element={<DestinationContent />} />
+          <Route path=":destination/" element={<DestinationContent />} />
         </Route>
         {/* Crew */}
-        <Route path="/React-Space-Router/crew" element={<CrewLayout />}>
+        <Route path="/React-Space-Router/crew/" element={<CrewLayout />}>
           <Route index element={<Navigate to="douglas" />} />
-          <Route path=":name" element={<CrewContent />} />
+          <Route path=":name/" element={<CrewContent />} />
         </Route>
         {/* Technology */}
-        <Route path="/React-Space-Router/technology" element={<TechnologyLayout />}>
+        <Route path="/React-Space-Router/technology/" element={<TechnologyLayout />}>
           <Route index element={<Navigate to="vehicle" />} />
-          <Route path=":technology" element={<TechnologyContent />} />
+          <Route path=":technology/" element={<TechnologyContent />} />
         </Route>
       </Routes>
     </>
