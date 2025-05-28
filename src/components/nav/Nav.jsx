1f2e29ba805b80b29d6555f0d5540933f9b81871
GoNEAR <cagatay.morgul@outlook.com>
2025-05-28 11:49:48 +0200

    bouton nav burger fonctionnelle

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,8 +3,10 @@ import logo from "../../assets/shared/logo.svg";
 import hamburger from "../../assets/shared/icon-hamburger.svg";
 import close from "../../assets/shared/icon-close.svg";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 export default function Nav() {
+  const [nav, setNav] = useState(false);
   return (
     <>
       <nav>
@@ -27,7 +29,19 @@ export default function Nav() {
             <NavLink to="/React-Space-Router/technology">03 TECHNOLOGY</NavLink>
           </li>
         </ul>
-        <button><img src={hamburger} alt="" /></button>
+        <button onClick={() => setNav(!nav)}>
+          <img src={hamburger} alt="" />
+        </button>
+        {nav && (
+          <div className="navResponsive">
+            <NavLink to="/React-Space-Router" end>
+              00 HOME
+            </NavLink>
+            <NavLink to="/React-Space-Router/destination">01 DESTINATION</NavLink>
+            <NavLink to="/React-Space-Router/crew">02 CREW</NavLink>
+            <NavLink to="/React-Space-Router/technology">03 TECHNOLOGY</NavLink>
+          </div>
+        )}
       </nav>
     </>
   );
